3e95505cbe9c714c42ec9f24fd7684837cdeb20e
ziv-perry <ziv.perry@zoominfo.com>
2025-01-13 19:46:14 +0200

    docs

--- mod.ts
+++ mod.ts
@@ -1,6 +1,23 @@
 /**
  * Read and write data from streams.
  * Supports ReadableStream, WritableStream, and Node.js streams.
+ *
+ * @example read from any readable stream
+ * ```ts
+ * import { read } from "@xpr/jsocket/read";
+ *
+ * const stream = getReadableStream();
+ * const data = await read(stream);
+ * ```
+ *
+ * @example write to any writable stream
+ * ```ts
+ * import { write } from "@xpr/jsocket/write";
+ *
+ * const stream = getWritableStream();
+ * await write(stream, data);
+ * ```
+ *
  * @module
  */
 import read from "./read.ts";
